be927d9b8f8839bc4d9d2a00f0571a6a1a7bb4a4
ZeyadRaafat <zeyadraafat65@gmail.com>
2025-06-26 10:50:31 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TokuApp());
+}
+
+class TokuApp extends StatelessWidget {
+  const TokuApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
